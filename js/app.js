c3ad897930e633965150676af345387cb2c6c8a0
bravodavid56 <bravodavid56@gmail.com>
2017-09-08 00:54:01 -0700

    Messed with themes, also switched to tabs instead of navbar

--- js/app.js
+++ js/app.js
@@ -1,28 +1,43 @@
 angular
     .module("myApp", ["ngRoute", "SignupCtrl", "ngMaterial"])
     .config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            template: "<p> hey check </p>",
-            controller: 'MainController'
-        })
-        .when("/aboutme", {
-            templateUrl: "views/aboutme.html"
-            // need a controller
-        })
-        .when("/projects", {
-            templateUrl: "views/projects.html"
-        })
-        .when("/contact", {
-            templateUrl: "views/contact.html"
-        })
-        .when("/main", {
-            templateUrl: "views/main.html",
-            controller: 'MainController'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
-}])
-
-    
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/aboutme.html",
+            })
+            .when("/aboutme", {
+                templateUrl: "views/aboutme.html"
+                // need a controller
+            })
+            .when("/projects", {
+                templateUrl: "views/projects.html"
+            })
+            .when("/contact", {
+                templateUrl: "views/contact.html"
+            })
+            .otherwise({
+                redirectTo: '/'
+            })
+    }])
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue-grey')
+            .accentPalette('amber')
+            .dark();
+           
+    })
+    .directive('renderMe', function() {
+        return {
+            templateUrl: 'views/aboutme.html'
+        }
+    })
+    .directive('renderProjects', function() {
+        return {
+            templateUrl: 'views/projects.html'
+        }
+    })
+    .directive('renderContact', function() {
+        return {
+            templateUrl: 'views/contact.html'
+        }
+    })
