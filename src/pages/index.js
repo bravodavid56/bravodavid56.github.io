9bbd5ccff67c064599be3bee86f88735890aeedf
Dave Bravo-Ruelas <Bravodavid56@gmail.com>
2021-06-26 19:12:09 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Dave Bravo-Ruelas
-    </Button>
+    <Container>
+      <Button variant="contained" color="primary">
+        Dave Bravo-Ruelas
+      </Button>
+    </Container>
   );
 }
 
